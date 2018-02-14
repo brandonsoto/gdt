01b17e9eedded4c2c87329595b33bfb09c5a57a5
Brandon Soto <brandon.soto09@gmail.com>
2018-02-14 11:39:38 -0800

    Implement get_service_pid()

--- gdt.py
+++ gdt.py
@@ -2,14 +2,16 @@ import argparse
 import subprocess
 import sys
 import os
+import nsync
+import re
 
-DEFAULT_TARGET_IP = "192.168.33.42"
+DEFAULT_TARGET_IP = "192.168.1.26"
 DEFAULT_TARGET_DEBUG_PORT = "8000"
-DEFAULT_TARGET_LOGIN = "vagrant"
-DEFAULT_TARGET_PASSWORD = "vagrant"
-DEFAULT_QNX_SDK = "D:\\Projects\\qnx600"
-DEFAULT_GDB = DEFAULT_QNX_SDK + "\\usr\\bin\\ntoarmv7-gdb.exe"
+DEFAULT_TARGET_PASSWORD = "#Pasa3Ford"
+DEFAULT_QNX_SDK = "D:\\Projects\\MY20\\QNX_SDK"
+DEFAULT_GDB = "D:\\qnx700\\host\\win64\\x86_64\\usr\\bin\\ntoarmv7-gdb.exe"
 DEFAULT_SYMBOLS = "D:\\Projects\\Symbols"
+SOLIB_SEARCH_PATH = 'D:\\Projects\\Symbols'
 
 RETURN_ERROR_FATAL = 1
 RETURN_ERROR_TEST_FAILURE = 2
@@ -19,30 +21,35 @@ command_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "command
 
 def run_gdb():
     try:
-        # TODO(brandon): change gdb when ready
-        ret = subprocess.call(["gdb", "--command=" + command_file])
+        ret = subprocess.call([DEFAULT_GDB, "--command=" + command_file])
         print "Debugging session ended successfully"
     except:
         subprocess.call("reset")
         print "Debugging session ended in an error"
         sys.exit(RETURN_ERROR_FATAL)
 
-def get_service_pid():
-    # TODO: implement using nsync
-    return 0
+def get_service_pid(service):
+    telnet = nsync.TelnetConnection(ip=DEFAULT_TARGET_IP, passwd=DEFAULT_TARGET_PASSWORD)
+    result = telnet.sendCommand("ps -A | grep " + service)
+    r = re.findall(r'\b\d+\b', result)
+    if len(r) > 0:
+        return r[0]
+    else:
+        return ""
 
 def generate_gdb_command_file(args):
     # TODO(brandon): need to add correct arguments to these commands
     file = open(command_file, 'w')
-    file.write('file ' + DEFAULT_SYMBOLS + '\n')
-    file.write('dir \n')
-    file.write('set solib-search-path \n')
-    file.write('set auto-solib-add on \n')
+    file.write('file ' + args.symbols + '\n')
+    # file.write('dir \n')
+    file.write('set sysroot ' + SOLIB_SEARCH_PATH + '\n')
+    file.write('set solib-search-path ' + SOLIB_SEARCH_PATH + '\n')
+    file.write('set auto-solib-add on\n')
     if args.core:
         file.write('core-file ' + args.core + '\n')
     else:
         file.write('target qnx ' + DEFAULT_TARGET_IP + ':' + DEFAULT_TARGET_DEBUG_PORT + '\n')
-        file.write('attach ' + str(get_service_pid()))
+        file.write('attach ' + get_service_pid(args.module) + '\n')
     file.close()
 
 def validate_args(args):
