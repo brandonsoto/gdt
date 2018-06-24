e33411d5a23562acc5144ffc1ae28d5d0a051bc1
Brandon Soto <brandon.soto09@gmail.com>
2018-06-24 09:33:47 -0700

    Parametize tests

--- gdt_files/tests/test_utilities.py
+++ gdt_files/tests/test_utilities.py
@@ -3,48 +3,76 @@ import gdt as g
 import os
 
 
-def test_validate_ipv4_address():
-    assert g.validate_ipv4_address('192.168.33.42') is not None
-    assert g.validate_ipv4_address('192.266.33.266') is None
-    assert g.validate_ipv4_address('192') is None
-    assert g.validate_ipv4_address('test') is None
+@pytest.mark.parametrize('test_input, expected', [
+    ('192.168.33.42', True),
+    ('192.168.3z.42', False),
+    ('192.266.33.266', False),
+    ('192', False),
+    ('test', False)
+])
+def test_validate_ipv4_address(test_input, expected):
+    assert (g.validate_ipv4_address(test_input) is not None) == expected
 
 
-def test_validate_port():
-    assert g.validate_port('0') is not None
-    assert g.validate_port('65535') is not None
-    assert g.validate_port('-1') is None
-    assert g.validate_port('65536') is None
+@pytest.mark.parametrize('test_input, expected', [
+    ('0', True),
+    ('65535', True),
+    ('-1', False),
+    ('65536', False)
+])
+def test_validate_port(test_input, expected):
+    assert (g.validate_port(test_input) is not None) == expected
 
 
-def test_validate_dir():
-    assert g.validate_dir(os.path.dirname(__file__)) is not None
-    assert g.validate_dir('bobo') is None
+@pytest.mark.parametrize('test_input, expected', [
+    (os.path.dirname(__file__), True),
+    ('bobo', False)
+])
+def test_validate_dir(test_input, expected):
+    assert (g.validate_dir(test_input) is not None) == expected
 
 
-def test_is_cpp_file():
-    for f in ("test.h", "test.cpp", "test.c", "test.cc", "test.hpp"):
-        assert g.is_cpp_file(f) is True
-    for f in ("test.py", "test.txt", "test"):
-        assert g.is_cpp_file(f) is False
+@pytest.mark.parametrize('test_input, expected', [
+    ('test.h', True),
+    ('test.cc', True),
+    ('test.cpp', True),
+    ('test.c', True),
+    ('test.cc', True),
+    ('test.hpp', True),
+    ('test.py', False),
+    ('test.txt', False),
+    ('test', False)
+])
+def test_is_cpp_file(test_input, expected):
+    assert g.is_cpp_file(test_input) == expected
 
 
-def test_is_shared_library():
-    for f in ("test.so", "test.so.42", "test.42.so"):
-        assert g.is_shared_library(f) is True
-    for f in ("test.py", "test.txt", "test", "test.a"):
-        assert g.is_shared_library(f) is False
+@pytest.mark.parametrize('test_input, expected', [
+    ('test.so', True),
+    ('test.so.42', True),
+    ('test.42.so', True),
+    ('test.py', False),
+    ('test.txt', False),
+    ('test.a', False),
+    ('test', False)
+])
+def test_is_shared_library(test_input, expected):
+    assert g.is_shared_library(test_input) == expected
 
 
 def test_extract_filename():
     assert g.extract_filename(__file__) == os.path.basename(__file__)[:-3]
 
 
-def test_get_str_repr():
-    assert g.get_str_repr('C:\Project\Test') == 'C:\\\\Project\\\\Test'
-    assert g.get_str_repr('C:/Project/Test') == 'C:/Project/Test'
-    assert g.get_str_repr('/Project/Test') == '/Project/Test'
-    assert g.get_str_repr('') == ''
+@pytest.mark.parametrize('test_input, expected', [
+    ('C:\Project\Test', 'C:\\\\Project\\\\Test'),
+    ('C:/Project/Test', 'C:/Project/Test'),
+    ('C:\Project/Test', 'C:\\\\Project/Test'),
+    ('TestStr', 'TestStr'),
+    ('', '')
+])
+def test_get_str_repr(test_input, expected):
+    assert g.get_str_repr(test_input) == expected
 
 
 def test_verify_required_files_exist_when_missing_config_dir():
@@ -58,10 +86,6 @@ def test_verify_required_files_exist_when_missing_config_dir():
 
 
 def test_verify_required_files_exist_when_missing_gdbinit_file(tmpdir):
-    old_dir = g.GDT_CONFIG_DIR
-    old_commands_file = g.CORE_COMMANDS_FILE
-    old_gdbinit = g.DEFAULT_GDBINIT_FILE
-
     g.GDT_CONFIG_DIR = tmpdir.strpath
     g.CORE_COMMANDS_FILE = tmpdir.join("corecommands").strpath
     g.DEFAULT_GDBINIT_FILE = tmpdir.join("gdbinit").strpath
@@ -71,16 +95,8 @@ def test_verify_required_files_exist_when_missing_gdbinit_file(tmpdir):
     with pytest.raises(g.RequiredFileMissing):
         g.verify_required_files_exist()
 
-    g.CORE_COMMANDS_FILE = old_commands_file
-    g.DEFAULT_GDBINIT_FILE = old_gdbinit
-    g.GDT_CONFIG_DIR = old_dir
-
 
 def test_verify_required_files_exist_when_missing_commands_file(tmpdir):
-    old_dir = g.GDT_CONFIG_DIR
-    old_commands_file = g.CORE_COMMANDS_FILE
-    old_gdbinit = g.DEFAULT_GDBINIT_FILE
-
     g.GDT_CONFIG_DIR = tmpdir.strpath
     g.CORE_COMMANDS_FILE = tmpdir.join("corecommands").strpath
     g.DEFAULT_GDBINIT_FILE = tmpdir.join("gdbinit").strpath
@@ -90,16 +106,8 @@ def test_verify_required_files_exist_when_missing_commands_file(tmpdir):
     with pytest.raises(g.RequiredFileMissing):
         g.verify_required_files_exist()
 
-    g.CORE_COMMANDS_FILE = old_commands_file
-    g.DEFAULT_GDBINIT_FILE = old_gdbinit
-    g.GDT_CONFIG_DIR = old_dir
-
 
 def test_verify_required_files_exist(tmpdir):
-    old_dir = g.GDT_CONFIG_DIR
-    old_commands_file = g.CORE_COMMANDS_FILE
-    old_gdbinit = g.DEFAULT_GDBINIT_FILE
-
     g.GDT_CONFIG_DIR = tmpdir.strpath
     g.CORE_COMMANDS_FILE = tmpdir.join("corecommands").strpath
     g.DEFAULT_GDBINIT_FILE = tmpdir.join("gdbinit").strpath
@@ -111,10 +119,6 @@ def test_verify_required_files_exist(tmpdir):
         g.verify_required_files_exist()
     except Exception as err:
         pytest.fail("Unexpected error: " + err.message)
-
-    g.CORE_COMMANDS_FILE = old_commands_file
-    g.DEFAULT_GDBINIT_FILE = old_gdbinit
-    g.GDT_CONFIG_DIR = old_dir
 
 
 def test_target_class():
