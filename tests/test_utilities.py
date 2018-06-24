d968e6722f5c9e2a330300e16c42083cf06d972f
Brandon Soto <brandon.soto09@gmail.com>
2018-06-24 16:14:19 -0700

    Add more unit tests

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -121,7 +121,7 @@ class TestUtilities(object):
 
 
 class TestConfigGenerator(object):
-    def test_configgenerator(self, mocker, tmpdir):
+    def test_configgenerator(self, mocker):
         mock_open = mocker.patch('__builtin__.open')
         mock_dump = mocker.patch('json.dump')
         mocker.patch('gdt.ConfigFileOption', autospec=True)
@@ -131,4 +131,46 @@ class TestConfigGenerator(object):
         mock_dump.assert_called_once()
         mock_open.assert_any_call(gdt.GDT_CONFIG_FILE, 'w')
         mock_open.assert_any_call(gdt.GDBINIT_FILE, 'w')
+
+class TestConfigFileOption(object):
+    @pytest.fixture
+    def config_file_option(self):
+        return gdt.ConfigFileOption(key="key", ask_user=False)
+
+    @pytest.mark.parametrize('default_value, value, validate_func, expected', [
+        ("default_value", "", lambda value: None, 'default_value'),
+        ("default_value", "value", lambda value: 'value', 'value')
+    ])
+    def test_init_value(self, mocker, config_file_option, default_value, value, validate_func, expected):
+        mock_input = mocker.patch('__builtin__.raw_input', return_value=value)
+        config_file_option.ask_user = True
+        config_file_option.default_value = default_value
+        config_file_option.validate_func = validate_func
+
+        config_file_option.init_value()
+
+        mock_input.assert_called_once()
+        assert config_file_option.value == expected
+
+    def test_init_value_no_input(self, mocker, config_file_option):
+        mock_input = mocker.patch('__builtin__.raw_input', return_value='bad_value')
+        config_file_option.ask_user = False
+
+        config_file_option.init_value()
+
+        mock_input.assert_not_called()
+        assert config_file_option.value is None
+
+    @pytest.mark.parametrize('default_value, initial_desc, expected', [
+        ("value", "key", "key [Default: \"value\"]: "),
+        ("", "key", "key: ")
+    ])
+    def test_init_desc(self, config_file_option, default_value, initial_desc, expected):
+        config_file_option.default_value = default_value
+        config_file_option.desc = initial_desc
+
+        config_file_option.init_desc()
+
+        assert config_file_option.desc == expected
+
 
